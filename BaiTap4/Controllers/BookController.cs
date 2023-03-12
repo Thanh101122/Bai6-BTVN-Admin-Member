014454c1a774195f33ce27365928405d9845d571
ADMIN <ADMIN@LAPTOP-RV2K0VLH>
2023-03-12 10:44:43 +0700

    Add project files.

---
+++ BaiTap4/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using BaiTap4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap4.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            var context = new BookModelContext();
+            return View(context.Books.ToList());
+        }
+        public ActionResult GetBookByCategory(int id)
+        {
+            var context = new BookModelContext();
+            return View("Index", context.Books.Where(p => p.CategoryId == id).ToList());
+        }
+        public ActionResult GetCategory()
+        {
+            var context = new BookModelContext();
+            var listCategory = context.Categories.ToList();
+            return PartialView(listCategory);
+        }
+        public ActionResult Details(int id)
+        {
+            var context = new BookModelContext();
+            var firstBook = context.Books.FirstOrDefault(p => p.Id == id);
+            if (firstBook == null)
+                return HttpNotFound("Không tìm thấy mã sách này!");
+            return View(firstBook);
+        }
+
+        [Authorize]
+        public ActionResult AddToCart(int id)
+        {
+            return Content("thêm thành công!!");
+        }
+    }
+}
---
+++ BaiTap4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaiTap4.Startup))]
+namespace BaiTap4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
